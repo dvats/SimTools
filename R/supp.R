a35ab383dbe1ce0ac8da965c474973d27667563e
Dootika Vats <dootika1990@gmail.com>
2020-11-28 23:57:53 +0530

    Prepping initial export.

--- R/supp.R
+++ R/supp.R
@@ -26,23 +26,16 @@ CIz <- function(z, p1 , p2, theta.hat, phi, ci.sigma.mat, n, mean = TRUE)
   return (list("lower.ci" = c(lower.ci.p1, lower.ci.p2), "upper.ci" = c(upper.ci.p1, upper.ci.p2)))
 }
 
-"set.mfrow" <-function (Nparms = 1, nplots = 1) 
+"set.mfrow" <-function (Nparms = 1) 
 {
   ## Set up dimensions of graphics window: 
-  ## If only density plots OR trace plots are requested, dimensions are: 
   ##	1 x 1	if Nparms = 1 
   ##	1 X 2 	if Nparms = 2 
   ##	2 X 2 	if Nparms = 3 or 4 
   ##	3 X 2 	if Nparmss = 5 or 6 or 10 - 12 
   ##	3 X 3 	if Nparms = 7 - 9 or >= 13 
-  ## If both density plots AND trace plots are requested, dimensions are: 
-  ##	1 x 2	if Nparms = 1 
-  ##	2 X 2 	if Nparms = 2 
-  ##	3 X 2 	if Nparms = 3, 4, 5, 6, 9 
-  ##	4 x 2	if Nparms otherwise 
-  if (nplots==1) {
     ## One plot per variable
-    mfrow <- switch(min(Nparms,13),
+  mfrow <- switch(min(Nparms,13),
                     c(1,1),
                     c(1,2),
                     c(2,2),
@@ -57,28 +50,10 @@ CIz <- function(z, p1 , p2, theta.hat, phi, ci.sigma.mat, n, mean = TRUE)
                     c(3,2),
                     c(3,3))
   }
-  else {
-    ## Two plot per variable
-    ##
-    mfrow <- switch(min(Nparms, 13),
-                    c(1,2),
-                    c(2,2),
-                    c(3,2),
-                    c(3,2),
-                    c(3,2),
-                    c(3,2),
-                    c(4,2),
-                    c(4,2),
-                    c(3,2),
-                    c(4,2),
-                    c(4,2),
-                    c(4,2),
-                    c(4,2))
-  }
   return(mfrow)
 }
 
-den.plot <- function(x, mn, quans, mcil,mciu, qcil, qciu, mean = TRUE,bord = NA,mean.color, quan.color,main = "Component-wise Densities",...)
+den.plot <- function(x, mn, quans, mcil, mciu, qcil, qciu, mean = TRUE,bord = NA,mean.color, quan.color,main = "Component-wise Densities",...)
 {
   plot(density(x, ...),main = main,...)
   if(mean){
@@ -129,7 +104,7 @@ plot.CIs <- function(x,dimn, CIs, bord = NULL, mean.color, quan.color, mean = TR
     
     
     if (auto.layout) {
-      mfrow <- set.mfrow(Nparms = 6, nplots = 1)
+      mfrow <- set.mfrow(Nparms = 6)
       pars <- par(mfrow = mfrow)
     }
     
@@ -147,18 +122,18 @@ plot.CIs <- function(x,dimn, CIs, bord = NULL, mean.color, quan.color, mean = TR
 #Q is list of quantiles to be estimated. It will be estimated for each component, m is the length of Q
 error.est <- function(x, Q = c(0.1, 0.9), alpha = 0.05, thresh, mean = TRUE,iid, ...) 
 {
-  (m = length(Q))
+  m <- length(Q)
   x <- as.matrix(x)
-  (n <- length(x[, 1]))
+  n <- length(x[, 1])
   if(!is.matrix(x) && !is.data.frame(x))
     stop("'x' must be a matrix or data frame.")
   #p1 is the dimension of g(x)
   #p2 is defined this because p1+p2 will ncols in lambda and sigma
   #v is the vector of all means and quantiles to be estimated
-  (p1 <- length(x[1,]))
-  (p2 <- m*length(x[1,]))
-  (theta.hat <- colMeans(x)) #g bar
-  (xi.q <- apply(x, 2, quantile, Q))
+  p1 <- length(x[1,])
+  p2 <- m*length(x[1,])
+  theta.hat <- colMeans(x) #g bar
+  xi.q <- apply(x, 2, quantile, Q)
   
   #phi is the vector of all quantiles
   phi <- rep(0, p2)
@@ -172,11 +147,11 @@ error.est <- function(x, Q = c(0.1, 0.9), alpha = 0.05, thresh, mean = TRUE,iid,
   {
     for(i in 1:(p2/m))
     {
-      (fs[(j-1)*(p2/m) + i] <- density(x[, i], from = xi.q[j, i], to = xi.q[j, i], n = 1 )$y )
+      fs[(j-1)*(p2/m) + i] <- density(x[, i], from = xi.q[j, i], to = xi.q[j, i], n = 1 )$y 
     }
   }
   
-  (I.flat <- rep(1, p1))
+  I.flat <- rep(1, p1)
   
   #since p2 was m*dim(h(x))
   lower.ci.mat <- matrix(0, nrow = m, ncol = p2/m)
@@ -185,7 +160,7 @@ error.est <- function(x, Q = c(0.1, 0.9), alpha = 0.05, thresh, mean = TRUE,iid,
   for(i in 1:m)
   {
     
-    (indi <- (apply(x, 1, Indicator, xi.q[i,])))
+    indi <- (apply(x, 1, Indicator, xi.q[i,]))
     if(p2 > 1)
     {
       indi <- t(indi)
@@ -198,25 +173,26 @@ error.est <- function(x, Q = c(0.1, 0.9), alpha = 0.05, thresh, mean = TRUE,iid,
   
   if(mean == FALSE) lambda <- 1/fs else (lambda <- 1/c(I.flat, fs))
   
-  (ci.sigma.mat <- (t(t(sigma.mat)*lambda))*lambda)
-  (p <- p1 + p2)
+  ci.sigma.mat <- (t(t(sigma.mat)*lambda))*lambda
+  p <- p1 + p2
   if(mean == FALSE) p = p2
-  (z1 <- qnorm(1 - alpha/2))
-  (z2 <- qnorm(1 - alpha/(2*p)))
-  (foo1 <- CIz(z1, p1, p2, theta.hat, phi,ci.sigma.mat, n, mean))
-  (foo2 <- CIz(z2,p1, p2, theta.hat, phi,ci.sigma.mat, n, mean))
-  if(mean == FALSE) v <- phi else (v <- c(theta.hat, phi))
+  
+  z1 <- qnorm(1 - alpha/2)
+  z2 <- qnorm(1 - alpha/(2*p))
+  foo1 <- CIz(z1, p1, p2, theta.hat, phi,ci.sigma.mat, n, mean)
+  foo2 <- CIz(z2,p1, p2, theta.hat, phi,ci.sigma.mat, n, mean)
+  if(mean == FALSE) v <- phi else v <- c(theta.hat, phi)
   
   count <- 0
-  (prob1 <- pmvnorm(lower = foo1$lower.ci, upper = foo1$upper.ci, mean = v, sigma = (ci.sigma.mat/n))[1])
-  (prob2 <- pmvnorm(lower = foo2$lower.ci, upper = foo2$upper.ci, mean = v, sigma = (ci.sigma.mat/n))[1])
+  prob1 <- pmvnorm(lower = foo1$lower.ci, upper = foo1$upper.ci, mean = v, sigma = (ci.sigma.mat/n))[1]
+  prob2 <- pmvnorm(lower = foo2$lower.ci, upper = foo2$upper.ci, mean = v, sigma = (ci.sigma.mat/n))[1]
   
   while(prob2 - prob1 > thresh)
   {
     count <- count + 1
-    (z.star <- (z1 + z2)/2)
-    (foo.star <- CIz(z.star, p1, p2, theta.hat, phi, ci.sigma.mat, n, mean))
-    (prob.star <- pmvnorm(lower = foo.star$lower.ci, upper = foo.star$upper.ci, mean = v, sigma = (ci.sigma.mat/n))[1])
+    z.star <- (z1 + z2)/2
+    foo.star <- CIz(z.star, p1, p2, theta.hat, phi, ci.sigma.mat, n, mean)
+    prob.star <- pmvnorm(lower = foo.star$lower.ci, upper = foo.star$upper.ci, mean = v, sigma = (ci.sigma.mat/n))[1]
     if(prob.star > 1- alpha) 
     {
       z2 <- z.star
@@ -232,7 +208,7 @@ error.est <- function(x, Q = c(0.1, 0.9), alpha = 0.05, thresh, mean = TRUE,iid,
       break
     }
   }
-  (temp <- CIz(z1, p1, p2, theta.hat, phi,ci.sigma.mat, n, mean))
+  temp <- CIz(z1, p1, p2, theta.hat, phi,ci.sigma.mat, n, mean)
   
   for(i in 1:m)
   {
@@ -259,14 +235,4 @@ error.est <- function(x, Q = c(0.1, 0.9), alpha = 0.05, thresh, mean = TRUE,iid,
   
   return(foo3)
   
-}
-
-plot.Smcmc <- function(x, Q = c(0.1, 0.9), alpha = 0.05,thresh = 0.001, iid = FALSE, plot = TRUE, auto.layout = TRUE, ask = dev.interactive(),mean = TRUE, border = NA, mean.col = 'plum4', quan.col = 'lightsteelblue3', opaq = 0.7, ...)
-{
-  foo3 <- error.est(x, Q, alpha,thresh = thresh,iid = iid,mean = mean, ...)
-  if(plot == TRUE)
-  {
-    plot.CIs(x, dimn = length(x[1,]), CIs = foo3, bord = border, mean.color = adjustcolor(mean.col, alpha.f = opaq), quan.color = adjustcolor(quan.col, alpha.f = opaq), mn = foo3$mean.est,mean = mean, quans = foo3$xi.q,auto.layout = auto.layout,ask = ask,...)
-  }
-  return(foo3)
 }

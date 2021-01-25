6c3d850e3408aafa032b076b311b1f51a57e1d5b
Gunjan Jalori <39437795+gunjanj007@users.noreply.github.com>
2021-01-25 23:20:20 +0530

    Update supp.R

--- R/supp.R
+++ R/supp.R
@@ -55,12 +55,12 @@ CIz <- function(z, p1 , p2, theta.hat, phi, ci.sigma.mat, n, mean = TRUE)
 
 
 addCI <- function(data, CIs, component = 1, bord = NA, 
-                  mean = TRUE,mean.col = 'plum4', quan.col = 'lightsteelblue3'
+                  mean = TRUE,mean.color = 'plum4', quan.color = 'lightsteelblue3'
                   , opaq = 0.7, ...)
 {
   x = ts(data[, component])
-  mean.color = adjustcolor(mean.col, alpha.f = opaq)
-  quan.color = adjustcolor(quan.col, alpha.f = opaq)
+  mean.color = adjustcolor(mean.color, alpha.f = opaq)
+  quan.color = adjustcolor(quan.color, alpha.f = opaq)
   mn <- CIs$mean.est[component]
   quans <- CIs$xi.q[ , component]
   mcil = CIs$lower.ci.mean[component]
@@ -278,7 +278,7 @@ makeCI <- function(x, Q = c(0.1, 0.9), alpha = 0.05, thresh = 0.001,
 
 
 ## For boxplots
-plot.boxx <- function(x, dimn, CIs, mean.color, quan.color, mn, quans, range, width, varwidth, notch, outline, plot, border,
+plot.boxx <- function(x, dimn, CIs, mean.color, quan.color, range, width, varwidth, notch, outline, plot, border,
                       col, ann, horizontal, add,...) 
 {
   if(is.null(attributes(x)$varnames)) 
@@ -290,26 +290,34 @@ plot.boxx <- function(x, dimn, CIs, mean.color, quan.color, mn, quans, range, wi
   boxplot.matrix(x, ..., range = range, width = width, varwidth = varwidth, notch = notch, outline = outline,
                  names=varnames, plot = plot, border = border, col = col, ann = ann, horizontal = horizontal, add = add)
   for(i in 1:dimn) {
-    quansi <- quans[, i]
-    qcil = CIs$lower.ci.mat[, i]
-    qciu = CIs$upper.ci.mat[, i]
-    for(j in 1:length(quansi))
-    {
-      if(horizontal==TRUE) {
-        polygon(c(qcil[j],qcil[j],qciu[j],qciu[j]), c(i-(0.2*min(dimn,2)),i+(0.2*min(dimn,2)),i+(0.2*min(dimn,2)), i-(0.2*min(dimn,2))),  col = quan.color, border = FALSE)
-      }else {
-        polygon(c(i-(0.2*min(dimn,2)),i+(0.2*min(dimn,2)),i+(0.2*min(dimn,2)), i-(0.2*min(dimn,2))), c(qcil[j],qcil[j],qciu[j],qciu[j]), col = quan.color, border = FALSE)
-      } 
-    } 
-    for(j in 1:length(quansi))
-    {
-      if(horizontal==TRUE) {
-        segments(quansi[j],i-(0.2*min(dimn,2)), quansi[j], i+(0.2*min(dimn,2)))
-      }else {
-        segments(i-(0.2*min(dimn,2)), quansi[j], i+(0.2*min(dimn,2)), quansi[j])
-      }
-      
-    }
+    boxCI(x, CI = CIs, component = i,dimn = dimn, mean.color=mean.color, 
+          quan.color = quan.color, horizontal = horizontal)
   }
 }
 
+boxCI <- function(x,CI,component = 1,dimn = 1,mean.color = 'plum4', quan.color = 'lightsteelblue3',horizontal = FALSE) 
+{
+  quans = CI$xi.q
+  mn = CI$mean.est
+  quansi <- quans[, component]
+  qcil = CI$lower.ci.mat[, component]
+  qciu = CI$upper.ci.mat[, component]
+  i <- component
+  for(j in 1:length(quansi))
+  {
+    if(horizontal==TRUE) {
+      polygon(c(qcil[j],qcil[j],qciu[j],qciu[j]), c(i-(0.2*min(dimn,2)),i+(0.2*min(dimn,2)),i+(0.2*min(dimn,2)), i-(0.2*min(dimn,2))),  col = quan.color, border = FALSE)
+    }else {
+      polygon(c(i-(0.2*min(dimn,2)),i+(0.2*min(dimn,2)),i+(0.2*min(dimn,2)), i-(0.2*min(dimn,2))), c(qcil[j],qcil[j],qciu[j],qciu[j]), col = quan.color, border = FALSE)
+    } 
+  } 
+  for(j in 1:length(quansi))
+  {
+    if(horizontal==TRUE) {
+      segments(quansi[j],i-(0.2*min(dimn,2)), quansi[j], i+(0.2*min(dimn,2)))
+    }else {
+      segments(i-(0.2*min(dimn,2)), quansi[j], i+(0.2*min(dimn,2)), quansi[j])
+    }
+    
+  }
+}

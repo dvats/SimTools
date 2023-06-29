9177a4e25904cd5d1b7acd55c23cf11987e10e46
Siddharth Pathak <110186062+SiddharthanilPathak@users.noreply.github.com>
2023-06-29 18:26:41 +0530

    Add files via upload

---
+++ R/Siidclass.R
@@ -0,0 +1,168 @@
+#' @title Siid class
+#'
+#' @description Class for independent and identically distributed (iid) samples
+#'
+#' @name Siid
+#' @aliases Siid as.Siid as.Siid.default is.iid 
+#' @usage Siid(data, varnames = colnames(data))
+#' @param data : an iid output matrix with nsim rows and p columns
+#' @param varnames : a character string equal to the number of columns in \code{data}
+#'
+#' @return an Siid class object
+#' @examples
+#' # Generating iid data
+#' chain <- matrix(rnorm(3*1e3), nrow = 1e3, ncol = 3)
+#' siid.obj <- Siid(chain)
+#'
+#' @export
+"Siid" <- function(data,
+                   varnames = colnames(data)) # make Siid object
+{
+  if(missing(data))
+    stop("Data must be provided.")
+
+  if(is.vector(data))
+    data <- as.matrix(data, ncol = 1)
+
+  nsim <- dim(data)[1]
+  out <- list( data  = data,
+               nsim     = nsim,
+               varnames = varnames )
+  
+  class(out) <- "Siid"
+  return(out)
+}
+
+"is.Siid" <- function(x) 
+{
+  if (inherits(x, "Siid")) 
+    return(TRUE)
+  return(FALSE)
+}
+
+#' @export
+"as.Siid" <- function (x, ...) 
+  UseMethod("as.Siid")
+
+#' @export
+"as.Siid.default" <- function (x, ...) 
+  if (is.Siid(x)) x else Siid(x)
+
+
+#' @title Boxplot for Siid
+#' @name boxplot.Siid
+#' @description Boxplots with simultaenous error bars around all quantiles for iid data.
+#' @usage \method{boxplot}{Siid}(x, ...,  alpha = 0.05, thresh = 0.001, 
+#'                      quan.col = 'lightsteelblue3', opaq = .6, range = 1.5, 
+#'                      width = NULL, varwidth = FALSE, outline = TRUE, plot = TRUE, 
+#'                      border = par("fg"), col = 'white', ann = !add, 
+#'                      horizontal = FALSE, add = FALSE)
+#' 
+#' @param x : a `Siid' class object
+#' @param ... : arguments sent to boxplot  
+#' @param alpha : confidence level of simultaneous confidence intervals 
+#' @param thresh : numeric typically less than .005 for the accuracy of the simulteaneous procedure
+#' @param quan.col : color for the quantile confidence intervals
+#' @param opaq : opacity of \code{mean.col} and \code{quan.col}. A value of 0 is transparent and 1 is completely opaque.
+#' @param range :  as defined for base \code{boxplot}
+#' @param width : as defined for base \code{boxplot}
+#' @param varwidth : as defined for base \code{boxplot}
+#' @param outline : as defined for base \code{boxplot}
+#' @param plot : logical indicating whether the plot is to be constructed
+#' @param border : as defined for base \code{boxplot}
+#' @param col : as defined for base \code{boxplot}
+#' @param ann : as defined for base \code{boxplot}  
+#' @param horizontal : as defined for base \code{boxplot}  
+#' @param add : as defined for base \code{boxplot}    
+#' @return returns the base \code{boxplot} with simultaneous confidence intervals around 
+#'         all quantiles
+#' @examples
+#' # Generating iid data
+#' chain <- matrix(rnorm(3*1e3), nrow = 1e3, ncol = 3)
+#' siid.obj <- Siid(chain)
+#' boxplot(siid.obj)
+#'
+#' @references
+#' Robertson, N., Flegal, J. M., Vats, D., and Jones, G. L., 
+#' “Assessing and Visualizing Simultaneous Simulation Error”, 
+#' Journal of Computational and Graphical Statistics,  2020. 
+#'
+#' @export
+"boxplot.Siid" <- function(x, ...,alpha = 0.05, thresh = 0.001,
+                      quan.col = 'lightsteelblue3', opaq = .6, range = 1.5, width = NULL, varwidth = FALSE,
+                      outline = TRUE, plot = TRUE, border = par("fg"), col = 'white',
+                      ann = !add, horizontal = FALSE, add = FALSE)
+{
+  x <- as.Siid(x)
+  Q <- c(0.25, 0.50, 0.75)
+  notch <- FALSE
+  foo3 <- getCI(x, Q, alpha = alpha, thresh = thresh, mean = FALSE, iid = TRUE)
+  plot.boxx(x, dimn = length(x$data[1,]), CIs = foo3, 
+            quan.color = adjustcolor(quan.col, alpha.f = opaq), 
+            range = range, width = width, varwidth = varwidth, notch = notch,
+            outline = outline,plot = plot, border = border, col = col, 
+            ann = ann, horizontal = horizontal, add = add,...)
+}
+
+
+#' @title Plot Siid
+#'
+#' @description Density plots with simultaenous error bars around means and quantiles
+#'  for iid data. 
+#'
+#'
+#' @name plot.Siid
+#' @usage \method{plot}{Siid}(x, Q = c(0.1, 0.9), alpha = 0.05, thresh = 0.001, 
+#'                            rug = TRUE, plot = TRUE, mean = TRUE, border = NA,
+#'                            mean.col = 'plum4', quan.col = 'lightsteelblue3', 
+#'                            opaq = 0.7, auto.layout = TRUE, 
+#'                            ask = dev.interactive(), ...)
+#' @param x : a `Siid' class object
+#' @param Q : vector of quantiles
+#' @param alpha : confidence level of simultaneous confidence intervals 
+#' @param thresh : numeric typically less than .005 for the accuracy of the simulteaneous procedure
+#' @param rug : logical indicating whether a rug plot is desired
+#' @param plot :  logical argument for is plots are to be returned 
+#' @param mean : logical argument whether the mean is to be plotted
+#' @param border : whether a border is required for the simultaneous confidence intervals
+#' @param mean.col : color for the mean confidence interval
+#' @param quan.col : color for the quantile confidence intervals
+#' @param opaq : opacity of \code{mean.col} and \code{quan.col}. A value of 0 is transparent and 1 is completely opaque.
+#' @param auto.layout : logical argument for an automatic layout of plots
+#' @param ask : activating interactive plots
+#' @param which : if one want to plots specific components
+#' @param ... : arguments passed on to the \code{density} plot in base R
+#' @return returns a plot of the univariate density estimates with simultaneous
+#'      confidence intervals wherever asked. If \code{plot == FALSE} a list of
+#'      estimates and simultaneous confidence intervals.
+#' @examples
+#' # Generating iid data
+#' chain <- matrix(rnorm(3*1e3), nrow = 1e3, ncol = 3)
+#' siid.obj <- Siid(chain)
+#' plot(siid.obj)
+#'
+#' @references
+#' Robertson, N., Flegal, J. M., Vats, D., and Jones, G. L., 
+#' “Assessing and Visualizing Simultaneous Simulation Error”, 
+#' Journal of Computational and Graphical Statistics,  2020. 
+#'
+#' @export
+"plot.Siid" <- function(x, Q = c(0.1, 0.9), alpha = 0.05, thresh = 0.001, rug = TRUE, 
+  plot = TRUE,  mean = TRUE, border = NA, mean.col = 'plum4', quan.col = 'lightsteelblue3',
+ opaq = 0.7, auto.layout = TRUE, ask = dev.interactive(),which = NULL, ...)
+{
+  x <- as.Siid(x)
+  out <- getCI(x, Q, alpha, thresh = thresh, iid = TRUE, mean = mean)
+  if(plot == TRUE)
+  {
+    plot.CIs(x, which, CIs = out, rug = rug, bord = border, 
+             mean.color = adjustcolor(mean.col, alpha.f = opaq), 
+             quan.color = adjustcolor(quan.col, alpha.f = opaq), 
+             mean = mean, auto.layout = auto.layout,
+             ask = ask, ...)
+  }
+  invisible(out)
+}
+
+
+
---
+++ R/Smcmcclass.R
@@ -0,0 +1,248 @@
+## usethis namespace: start
+#' @importFrom grDevices adjustcolor dev.interactive
+#' @importFrom graphics boxplot par polygon segments boxplot.matrix
+#' @importFrom stats cov density qnorm quantile ts
+#' @importFrom mcmcse mcse.multi batchSize
+#' @importFrom mvtnorm pmvnorm
+## usethis namespace: end
+
+#' @title Smcmc class
+#'
+#' @description Smcmc class for simulated data using Markov chain Monte Carlo
+#'
+#' @name Smcmc
+#' @aliases Smcmc as.Smcmc as.Smcmc.default is.mcmc 
+#' @usage Smcmc(data, batch.size = TRUE, stacked = TRUE, varnames = colnames(data))
+#' @param data : a list of MCMC output matrices each with `nsim` rows and `p` columns
+#' @param batch.size : logical argument, if true, calculates the batch size appropriate for this Markov chain. Setting to TRUE saves time in future steps.
+#' @param stacked : recommended to be `TRUE`. logical argument, if true, stores a carefully stacked version of the MCMC output for use later.
+#' @param varnames : a character string equal to the number of columns in \code{data}
+#'
+#' @return an Smcmc class object
+#' @examples
+#' # Producing Markov chain
+#' chain <- matrix(0, nrow = 1e3, ncol = 1)
+#' chain[1,] <- 0
+#' err <- rnorm(1e3)
+#' for(i in 2:1e3)
+#' {
+#'   chain[i,] <- .3*chain[i-1,] + err[i]
+#' }
+#' smcmc.obj <- Smcmc(chain)
+#' @export
+Smcmc <- function(data,
+                    batch.size = TRUE, 
+                    stacked = TRUE,
+                    varnames = NULL) # make Smcmc object
+{
+  if(missing(data))
+    stop("Data must be provided.")
+  
+  
+  if(!is.list(data))
+  {
+    data = as.matrix(data)
+    data <- list(data)
+  }else
+  {
+    for(i in 1:length(data))
+    {
+      data[[i]] = as.matrix(data[[i]])
+    }
+  }
+  
+  nsim <- dim(data[[1]])[1]
+  if(is.null(varnames)) varnames <- colnames(data[[1]])  
+  
+  if(stacked == TRUE)
+  {
+    foo <- chain_stacker(data)
+    stacked.chain <- foo$stacked.data
+    
+    if(batch.size == TRUE)
+    {
+      size <- foo$b.size
+    }
+    else{
+      size <- NULL
+    }
+  }
+  
+  out <- list( chains = data,
+               stacked  = stacked.chain,
+               b.size   = size,
+               nsim     = nsim,
+               varnames = varnames)
+  
+  class(out) <- "Smcmc"
+  return(out)
+}
+
+"is.Smcmc" <- function (x) 
+{
+  if (inherits(x, "Smcmc")) 
+    return(TRUE)
+  return(FALSE)
+}
+
+#' @export
+"as.Smcmc" <- function (x, ...) 
+  UseMethod("as.Smcmc")
+
+#' @export
+"as.Smcmc.default" <- function (x, ...) 
+  if (is.Smcmc(x)) x else Smcmc(x)
+
+
+
+
+#' @title density plot form Smcmc class
+#'
+#' @description Density plots with simultaenous error bars around means and quantiles
+#'  for MCMC data. The error bars account for the correlated nature of the process.
+#'
+#'
+#' @name densityplot
+#' @usage densityplot(x, Q = c(0.1, 0.9), alpha = 0.05, thresh = 0.001, iid = FALSE,
+#'                             plot = TRUE, mean = TRUE, border = NA, mean.col = 'plum4', 
+#'                             quan.col = 'lightsteelblue3',rug = TRUE, opaq = 0.7, 
+#'                             auto.layout = TRUE, ask = dev.interactive(),...)    
+#' @param x : a `Smcmc' class object
+#' @param Q : vector of quantiles
+#' @param alpha : confidence level of simultaneous confidence intervals 
+#' @param thresh : numeric typically less than .005 for the accuracy of the simulteaneous procedure
+#' @param main : To add main heading
+#' @param iid : logical argument for constructing density plot for iid samples. Defaults to \code{FALSE}
+#' @param plot :  logical argument for is plots are to be returned 
+#' @param mean : logical argument whether the mean is to be plotted
+#' @param which : A vector of components, if you want plots of specific components.
+#' @param border : whether a border is required for the simultaneous confidence intervals
+#' @param mean.col : color for the mean confidence interval
+#' @param quan.col : color for the quantile confidence intervals
+#' @param rug : logical indicating whether a rug plot is desired
+#' @param opaq : opacity of \code{mean.col} and \code{quan.col}. A value of 0 is transparent and 1 is completely opaque.
+#' @param auto.layout : logical argument for an automatic layout of plots
+#' @param ask : activating inter active plots
+#' @param ... : arguments passed on to the \code{density} plot in base R
+#' @return returns a plot of the univariate density estimates with simultaneous
+#'			confidence intervals wherever asked. If \code{plot == FALSE} a list of
+#'			estimates and simultaneous confidence intervals.
+#' @examples
+#' # Producing Markov chain
+#' chain <- matrix(0, ncol = 1, nrow = 1e3)
+#' chain[1,] <- 0
+#' err <- rnorm(1e3)
+#' for(i in 2:1e3)
+#' {
+#'   chain[i,] <- .3*chain[i-1,] + err[i]
+#' }
+#' chain <- Smcmc(list(chain))
+#' densityplot(chain)
+#'
+#' @references
+#' Robertson, N., Flegal, J. M., Vats, D., and Jones, G. L., 
+#' “Assessing and Visualizing Simultaneous Simulation Error”, 
+#' Journal of Computational and Graphical Statistics,  2020. 
+#'
+#' @export
+
+
+"densityplot" <- function(x, 
+                          Q        = c(0.1, 0.9), 
+                          alpha    = 0.05, 
+                          thresh   = 0.001, 
+                          main     = NA,
+                          iid      = FALSE, 
+                          plot     = TRUE,  
+                          mean     = TRUE,
+                          which    = NULL,
+                          border   = NA, 
+                          mean.col = 'plum4', 
+                          quan.col = 'lightsteelblue3',
+                          rug      = FALSE, 
+                          opaq     = 0.7, 
+                          auto.layout = TRUE, 
+                          ask      = dev.interactive(), ...)
+{
+  
+  x <- as.Smcmc(x)
+  out <- getCI(x, Q, alpha, thresh = thresh, iid = iid, mean = mean)
+  if(plot == TRUE)
+  {
+    plot.CIs(x, CIs = out, bord = border, 
+             mean.color = adjustcolor(mean.col, alpha.f = opaq), 
+             quan.color = adjustcolor(quan.col, alpha.f = opaq), 
+             mean = mean, auto.layout = auto.layout, rug = rug,
+             ask = ask,main = main,which= which, ...)
+  }
+  invisible(out)
+}
+
+
+#' @title Summary plot function for Smcmc objects
+#'
+#' @description Plots traceplot, acfplot and densityplot of all the dimensions of 
+#' chains in  Smcmc object
+#'
+#' @name plot.Smcmc
+#' @usage plot.Smcmc(x)    
+#' @param x : a `Smcmc' class object
+#' @return return plot(s) of the all the dimensions of Smcmc object
+#' @examples
+#' # Producing Markov chain
+#' chain <- matrix(0, ncol = 1, nrow = 1e3)
+#' chain[1,] <- 0
+#' err <- rnorm(1e3)
+#' for(i in 2:1e3)
+#' {
+#'   chain[i,] <- .3*chain[i-1,] + err[i]
+#' }
+#' chain <- Smcmc(list(chain))
+#' plot(chain)
+#'
+#'@export
+
+"plot.Smcmc" <- function(x,...)
+{
+  if(class(x)!="Smcmc"){stop("Argument must be Smcmc object")}
+  x = x$chains
+  dimn <- dim(x[[1]])
+  n <- dimn[1]
+  p <- dimn[2]
+  m <- length(x)
+  p2 <- 0
+  p3 <- 0
+  if(p>10){stop("Maximum allowed dimension is 10")}
+  if(p>4){p2 = p-4;p = 4}
+  if(p2 >4)
+  {
+    p3 <- p2-4
+    p2 <- 4
+  }
+  par(mfrow = c(3,p),mar = c(2,4,1,1))
+  traceplot(x,which = 1:p,legend = F,xlab = NA,...)
+  acfplot(x,which = 1:p,xlab = NA,...)
+  densityplot(x,which = 1:p,...)
+  
+  if(p2>0)
+  {
+    par(mfrow = c(3,p2),mar = c(2,4,1,2))
+    traceplot(x,which = 5:(4+p2),legend = F,xlab = NA)
+    acfplot(x,which = 5:(4+p2),xlab = NA)
+    densityplot(x,which = 5:(4+p2))
+  }
+  
+  if(p3>0)
+  {
+    par(mfrow = c(3,p3),mar = c(2,4,1,2))
+    traceplot(x,which = 9:(8+p3),legend = F,xlab = NA)
+    acfplot(x,which = 9:(8+p3),xlab = NA)
+    densityplot(x,which = 9:(8+p3))
+  }
+  on.exit(par(ask = FALSE,mfrow=c(1,1)))
+  par(mar = c(5.1, 4.1, 4.1, 2.1))
+  par(fig = c(0, 1, 0 , 1))
+  par(oma = c(0, 0, 0, 0))
+}
+
+

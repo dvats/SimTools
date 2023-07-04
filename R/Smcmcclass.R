509b02f4ee6c4cb7594b9826d47d793fef14f555
Siddharth Pathak <110186062+SiddharthanilPathak@users.noreply.github.com>
2023-07-05 01:28:04 +0530

    Add files via upload

---
+++ R/Smcmcclass.R
@@ -0,0 +1,402 @@
+## usethis namespace: start
+#' @importFrom grDevices adjustcolor dev.interactive
+#' @importFrom graphics boxplot par polygon segments boxplot.matrix
+#' @importFrom stats cov density qnorm quantile ts
+#' @importFrom mcmcse mcse.multi batchSize
+#' @importFrom mvtnorm pmvnorm
+## usethis namespace: end
+
+#' @title Smcmc class
+#'
+#' @description Smcmc class for simulated data using Markov chain Monte Carlo
+#'
+#' @name Smcmc
+#' @aliases Smcmc as.Smcmc as.Smcmc.default is.mcmc 
+#' @usage Smcmc(data, batch.size = TRUE, stacked = TRUE, varnames = colnames(data))
+#' @param data : a list of MCMC output matrices each with `nsim` rows and `p` columns
+#' @param batch.size : logical argument, if true, calculates the batch size appropriate for this Markov chain. Setting to TRUE saves time in future steps.
+#' @param stacked : recommended to be `TRUE`. logical argument, if true, stores a carefully stacked version of the MCMC output for use later.
+#' @param varnames : a character string equal to the number of columns in \code{data}
+#'
+#' @return an Smcmc class object
+#' @examples
+#' # Producing Markov chain
+#' chain <- matrix(0, nrow = 1e3, ncol = 1)
+#' chain[1,] <- 0
+#' err <- rnorm(1e3)
+#' for(i in 2:1e3)
+#' {
+#'   chain[i,] <- .3*chain[i-1,] + err[i]
+#' }
+#' smcmc.obj <- Smcmc(chain)
+#' @export
+Smcmc <- function(data,
+                    batch.size = TRUE, 
+                    stacked = TRUE,
+                    varnames = NULL) # make Smcmc object
+{
+  if(missing(data))
+    stop("Data must be provided.")
+  
+  
+  if(!is.list(data))
+  {
+    data = as.matrix(data)
+    data <- list(data)
+  }else
+  {
+    for(i in 1:length(data))
+    {
+      data[[i]] = as.matrix(data[[i]])
+    }
+  }
+  
+  nsim <- dim(data[[1]])[1]
+  if(is.null(varnames)) varnames <- colnames(data[[1]])  
+  
+  if(stacked == TRUE)
+  {
+    foo <- chain_stacker(data)
+    stacked.chain <- foo$stacked.data
+    
+    if(batch.size == TRUE)
+    {
+      size <- foo$b.size
+    }
+    else{
+      size <- NULL
+    }
+  }
+  
+  out <- list( chains = data,
+               stacked  = stacked.chain,
+               b.size   = size,
+               nsim     = nsim,
+               varnames = varnames)
+  
+  class(out) <- "Smcmc"
+  return(out)
+}
+
+"is.Smcmc" <- function (x) 
+{
+  if (inherits(x, "Smcmc")) 
+    return(TRUE)
+  return(FALSE)
+}
+
+#' @export
+"as.Smcmc" <- function (x, ...) 
+  UseMethod("as.Smcmc")
+
+#' @export
+"as.Smcmc.default" <- function (x, ...) 
+  if (is.Smcmc(x)) x else Smcmc(x)
+
+
+
+
+#' @title density plot form Smcmc class
+#'
+#' @description Density plots with simultaenous error bars around means and quantiles
+#'  for MCMC data. The error bars account for the correlated nature of the process.
+#'
+#'
+#' @name densityplot
+#' @usage densityplot(x, Q = c(0.1, 0.9), alpha = 0.05, thresh = 0.001, iid = FALSE,
+#'                             plot = TRUE, mean = TRUE, border = NA, mean.col = 'plum4', 
+#'                             quan.col = 'lightsteelblue3',rug = TRUE, opaq = 0.7, 
+#'                             auto.layout = TRUE, ask = dev.interactive(),...)    
+#' @param x : a `Smcmc' class object
+#' @param Q : vector of quantiles
+#' @param alpha : confidence level of simultaneous confidence intervals 
+#' @param thresh : numeric typically less than .005 for the accuracy of the simulteaneous procedure
+#' @param main : To add main heading
+#' @param iid : logical argument for constructing density plot for iid samples. Defaults to \code{FALSE}
+#' @param plot :  logical argument for is plots are to be returned 
+#' @param mean : logical argument whether the mean is to be plotted
+#' @param which : A vector of components, if you want plots of specific components.
+#' @param border : whether a border is required for the simultaneous confidence intervals
+#' @param mean.col : color for the mean confidence interval
+#' @param quan.col : color for the quantile confidence intervals
+#' @param rug : logical indicating whether a rug plot is desired
+#' @param opaq : opacity of \code{mean.col} and \code{quan.col}. A value of 0 is transparent and 1 is completely opaque.
+#' @param auto.layout : logical argument for an automatic layout of plots
+#' @param ask : activating inter active plots
+#' @param ... : arguments passed on to the \code{density} plot in base R
+#' @return returns a plot of the univariate density estimates with simultaneous
+#'			confidence intervals wherever asked. If \code{plot == FALSE} a list of
+#'			estimates and simultaneous confidence intervals.
+#' @examples
+#' # Producing Markov chain
+#' chain <- matrix(0, ncol = 1, nrow = 1e3)
+#' chain[1,] <- 0
+#' err <- rnorm(1e3)
+#' for(i in 2:1e3)
+#' {
+#'   chain[i,] <- .3*chain[i-1,] + err[i]
+#' }
+#' chain <- Smcmc(list(chain))
+#' densityplot(chain)
+#'
+#' @references
+#' Robertson, N., Flegal, J. M., Vats, D., and Jones, G. L., 
+#' “Assessing and Visualizing Simultaneous Simulation Error”, 
+#' Journal of Computational and Graphical Statistics,  2020. 
+#'
+#' @export
+
+
+"densityplot" <- function(x, 
+                          Q        = c(0.1, 0.9), 
+                          alpha    = 0.05, 
+                          thresh   = 0.001, 
+                          main     = NA,
+                          iid      = FALSE, 
+                          plot     = TRUE,  
+                          mean     = TRUE,
+                          which    = NULL,
+                          border   = NA, 
+                          mean.col = 'plum4', 
+                          quan.col = 'lightsteelblue3',
+                          rug      = FALSE, 
+                          opaq     = 0.7, 
+                          auto.layout = TRUE, 
+                          ask      = dev.interactive(), ...)
+{
+  
+  x <- as.Smcmc(x)
+  out <- getCI(x, Q, alpha, thresh = thresh, iid = iid, mean = mean)
+  if(plot == TRUE)
+  {
+    plot.CIs(x, CIs = out, bord = border, 
+             mean.color = adjustcolor(mean.col, alpha.f = opaq), 
+             quan.color = adjustcolor(quan.col, alpha.f = opaq), 
+             mean = mean, auto.layout = auto.layout, rug = rug,
+             ask = ask,main = main,which= which, ...)
+  }
+  invisible(out)
+}
+
+
+#' @title Summary plot function for Smcmc objects
+#'
+#' @description Plots traceplot, acfplot and densityplot of all the dimensions of 
+#' chains in  Smcmc object
+#'
+#' @name plot.Smcmc
+#' @usage plot.Smcmc(x)    
+#' @param x : a `Smcmc' class object
+#' @return return plot(s) of the all the dimensions of Smcmc object
+#' @examples
+#' # Producing Markov chain
+#' chain <- matrix(0, ncol = 1, nrow = 1e3)
+#' chain[1,] <- 0
+#' err <- rnorm(1e3)
+#' for(i in 2:1e3)
+#' {
+#'   chain[i,] <- .3*chain[i-1,] + err[i]
+#' }
+#' chain <- Smcmc(list(chain))
+#' plot(chain)
+#'
+#'@export
+
+"plot.Smcmc" <- function(x,which = NULL,...)
+{
+  if(class(x)!="Smcmc"){stop("Argument must be Smcmc object")}
+  y = x$chains
+  dimn <- dim(y[[1]])
+  n <- dimn[1]
+  p <- dimn[2]
+  m <- length(x)
+  p2 <- 0
+  p3 <- 0
+  if(!is.null(which)){p = length(which)}else{which = 1:p}
+  if(p>12){stop("Maximum allowed dimension is 12")}
+  if(p>4){p2 = p-4;p = 4}
+  if(p2 >4)
+  {
+    p3 <- p2-4
+    p2 <- 4
+  }
+  par(mfrow = c(3,p),oma = c(0,0,0,0),mar = c(2.2,4,1,1))
+  traceplot(x,which = which[1:p],legend = F,xlab = NA,...)
+  acfplot(x,which = which[1:p],xlab = NA,...)
+  densityplot(x,which = which[1:p],...)
+  
+  if(p2>0)
+  {
+    par(mfrow = c(3,p2),mar = c(2,4,1,2))
+    traceplot(x,which = which[5:(4+p2)],legend = F,xlab = NA)
+    acfplot(x,which = which[5:(4+p2)],xlab = NA)
+    densityplot(x,which = which[5:(4+p2)])
+  }
+  
+  if(p3>0)
+  {
+    par(mfrow = c(3,p3),mar = c(2,4,1,2))
+    traceplot(x,which = which[9:(8+p3)],legend = F,xlab = NA)
+    acfplot(x,which = which[9:(8+p3)],xlab = NA)
+    densityplot(x,which = which[9:(8+p3)])
+  }
+  on.exit(par(ask = FALSE,mfrow=c(1,1)))
+  par(mar = c(5.1, 4.1, 4.1, 2.1))
+  par(fig = c(0, 1, 0 , 1))
+  par(oma = c(0, 0, 0, 0))
+}
+
+
+#' @title Summary function for Smcmc objects
+#'
+#' @description To show different statistics of the Smcmc object
+#'
+#' @name summary.Smcmc
+#' @usage summary.Smcmc(x)    
+#' @param x : a `Smcmc' class object
+#' @return return statistics of the all the dimensions(& chains) in Smcmc object
+#' @examples
+#' # Producing Markov chain
+#' chain <- matrix(0, ncol = 1, nrow = 1e3)
+#' chain[1,] <- 0
+#' err <- rnorm(1e3)
+#' for(i in 2:1e3)
+#' {
+#'   chain[i,] <- .3*chain[i-1,] + err[i]
+#' }
+#' chain <- Smcmc(list(chain))
+#' summary(chain)
+#'
+#'@export
+
+
+"summary.Smcmc" <- function (object)
+{
+  object <- as.Smcmc(object)
+  object.class <- class(object)
+  Batch_Size = object$b.size
+  Smcmc_output <- object$chains[[1]]
+  chains <- object$chains
+  columns = ncol(Smcmc_output)
+  dim3 <- vector(length = length(chains))
+  for(i in 1:length(chains)){dim3[i] = paste("Chain:",i)}
+  dim2 <- vector(length = columns)
+  if(!is.null(object$varnames)){dim2 = object$varnames }
+  else{for(i in 1:columns){dim2[i] = paste("Component",i)}}
+  dim1 <- c("Mean","Median","Variance","Minimum","Maximum")
+  
+  statistics <- array(NA, dim= c(5,columns,length(chains)),dimnames = list(dim1,dim2,dim3))
+  
+  for(i in 1:length(chains))
+  {
+    statistics[1, ,i] = apply(chains[[i]],2,mean)
+    statistics[2, ,i] = apply(chains[[i]],2,median)
+    statistics[3, ,i] = apply(chains[[i]],2,var)
+    statistics[4, ,i] = apply(chains[[i]],2,min)
+    statistics[5, ,i] = apply(chains[[i]],2,max)
+  }
+  summary_list <- list(Class = object.class,
+                       Batch_Size = Batch_Size,
+                       Statistics = statistics)
+  return(summary_list)
+}
+
+
+
+
+
+#' @title Covert to Smcmc Object
+#'
+#' @description To covert different MCMC objects to Smcmc object
+#'
+#' @name convert2Smcmc
+#' @usage convert2Smcmc(x)    
+#' @param x : a object belongs from any of "mcmc.list", "stanfit", "rstan", "array", "matrix" classes.
+#' @return return Smcmc object having same chain(s)
+#' @examples
+#' 
+#' library(StanHeaders)
+#' library(rstan)
+#'
+#' stanmodelcode <- "
+#' data { 
+#'  int<lower=0> n;
+#'  vector[2] x[n];
+#'  }
+#'  parameters {
+#'  vector[2] mu;
+#'  vector<lower=0>[2] lambda;
+#'  real<lower=-1,upper=1> r;
+#'  } 
+#'  transformed parameters {
+#'  vector<lower=0>[2] sigma;
+#'  cov_matrix[2] T;
+#'  sigma[1] <- inv_sqrt(lambda[1]);
+#'  sigma[2] <- inv_sqrt(lambda[2]);
+#'  T[1,1] <- square(sigma[1]);
+#'  T[1,2] <- r * sigma[1] * sigma[2];
+#'  T[2,1] <- r * sigma[1] * sigma[2];
+#'  T[2,2] <- square(sigma[2]);
+#'  }
+#'  model {
+#'  mu ~ normal(0, inv_sqrt(.001));
+#'  lambda ~ gamma(.001, .001);
+#'  x ~ multi_normal(mu, T);
+#'  }"
+#'
+#'  x <- matrix(c(0.8,102,1.0,98,0.5,100,0.9,105,0.7,103,0.4,110, 1.2,99, 1.4,87,0.6,113,1.1,89,
+#'                 1.3,93), nrow=11, ncol=2, byrow=TRUE) 
+#'                 
+#'  n <- nrow(x) # number of people/units measured
+#'
+#' data <- list(x=x, n=n) # to be passed on to Stan
+#' myinits <- list(list(r=0))
+#'
+#' # parameters to be monitored: 
+#' parameters <- c("r")
+#'
+#' # The following command calls Stan with specific options.
+#' # For a detailed description type "?rstan".
+#' fit <- stan(model_code=stanmodelcode,data=data,init=myinits,iter=1000,chains=1,thin=1)
+#'
+#' convert2Smcmc(fit)
+#'
+#'@export
+
+
+convert2Smcmc <- function(x)
+{
+  if(class(x) =="mcmc.list")
+  {
+    temp = list(as.matrix(x[[1]]))
+    
+    for(i in 2:length(x))
+    {
+      append(temp,as.matrix(x[[i]]))
+    }
+    return(as.Smcmc(temp))
+  }
+  
+  else if("stanfit"%in%class(x) || "rstan"%in%class(x))
+  {
+    x = as.array(x)
+    dim_samples <- dim(x)
+    # Reshape the multidimensional array into separate chains
+    num_chains <- dim_samples[1]  # Number of chains
+    num_iterations <- dim_samples[2]  # Number of iterations
+    num_variables <- dim_samples[3]  # Number of variables
+    
+    chains = list(as.matrix(x[,1 , ]))
+    if(num_iterations > 1){
+      for (i in 2:num_iterations) {
+        append(chains, as.matrix(x[ ,i, ]))
+      }
+    }
+    return(as.Smcmc(chains))
+  }
+  
+  else if("array"%in%class(x) || "matrix"%in%class(x) ||"list"%in%class(x))
+  {
+    return(as.Smcmc(x))
+  }
+}
---
+++ R/supp.R
@@ -0,0 +1,199 @@
+## For simultaneous confidence intervals
+Indicator <- function(x, ...)
+{
+  return(as.numeric(x > ... ))
+}
+
+#since our goal is to optimize z, this function returns the Confidence interval
+#for a particular z
+
+CIz <- function(z, p1 , p2, theta.hat, phi, ci.sigma.mat, n, mean = TRUE)
+{
+  se_ests <- sqrt(diag(ci.sigma.mat)/n)
+  
+  if (mean == FALSE)
+  {
+    lower.ci.p2 <- phi - z * se_ests
+    upper.ci.p2 <- phi + z * se_ests 
+    return (list("lower.ci" = lower.ci.p2, "upper.ci" = upper.ci.p2))  
+  }
+  p <- p1 + p2
+  
+  lower.ci.p1 <- theta.hat - z * se_ests[1:p1]
+  upper.ci.p1 <- theta.hat + z * se_ests[1:p1] 
+  lower.ci.p2 <- phi - z * se_ests[(p1+1):p]
+  upper.ci.p2 <- phi + z * se_ests[(p1+1):p]
+  
+  return (list("lower.ci" = c(lower.ci.p1, lower.ci.p2), "upper.ci" = c(upper.ci.p1, upper.ci.p2)))
+}
+
+
+## for densityplot
+plot.CIs <- function(x, 
+                     CIs, 
+                     bord = NULL, 
+                     mean.color = 'plum4' , 
+                     quan.color = 'lightsteelblue3', 
+                     rug,
+                     main,
+                     mean = TRUE, 
+                     auto.layout, 
+                     ask,
+                     which, ...)
+{
+  if(class(x) == "Smcmc")
+  {
+    if(is.null(x$varnames)) 
+    {
+      varnames <- paste("Density of" ,as.character(1:dim(x$chains[[1]])[2]))
+    }else{
+      varnames <- x$varnames
+    }
+    data <- x$stacked
+  }else{
+    if(class(x) == "Siid")
+    {
+      if(is.null(x$varnames)) 
+      {
+        varnames <- paste("Density of" ,as.character(1:dim(x$data)[2]))
+      }else{
+        varnames <- x$varnames
+      }
+      data <- x$data
+    }
+  }
+  p = length(data[1,])
+  dimn = 1:p
+  if(!is.null(which)){dimn = which}
+  lay = par()
+  leg = lay$mfrow[1]*lay$mfrow[2]
+  if(is.null(which))
+  {
+    setLayout_trace(p)
+    if(is.na(main)){par(oma = c(1,0,1,0))}else{par(oma = c(1,0,2,0))}
+  }
+  for(i in dimn)
+  {
+    if(is.null(which)){par(mar = c(1.2, 4.1,1.2, 2.1))}
+    else if(!is.null(which) && leg == 1){par(mar = c(4,4,2,2))}
+    beta = data[, i]
+    if ( max(abs(beta - floor(beta))) == 0 || bndw(beta) == 0 || length(unique(beta)) == 1)
+    {
+      beta = as.vector(beta)
+      q = unique(beta)
+      tab = table(beta)
+      p = as.vector(tab)/sum(tab)
+      plot_pmf(q,p,varnames[i],main,which)
+    }
+    
+    else
+    {
+      beta = ts(beta)
+      plot(density(beta,...),main = if(is.null(which)){NA} else{main}, xlab = NA, ylab = varnames[i],lwd =2)
+      if(rug == TRUE) rug(beta, ticksize=0.03, side=1, lwd=0.5)
+      addCI(x, CIs, component = i, bord = bord, 
+            mean.color = mean.color, quan.color = quan.color, 
+            mean = mean)
+    }
+  }
+  if(!is.na(main)&&is.null(which)){mtext(main, side = 3, line = 0.05,outer = TRUE, cex = 1.5)}
+  if(leg == 1 || is.null(which))
+  {
+    on.exit(par(ask = FALSE,mfrow=lay$mfrow))
+    par(mar = c(5.1, 4.1, 4.1, 2.1))
+    par(fig = c(0, 1, 0 , 1))
+    par(oma = c(0, 0, 0, 0))
+  }
+  
+}
+
+
+## For boxplots
+plot.boxx <- function(x, dimn, CIs, quan.color, range, width, varwidth, notch, outline, plot, border,
+                      col, ann, horizontal, add,...) 
+{
+  if(is.null(x$varnames)) 
+  {
+    varnames <- as.character(1:dim(x$data)[2])
+  }else{
+    varnames <- x$varnames
+  }
+  boxplot.matrix(x$data, ..., range = range, width = width, varwidth = varwidth, notch = notch, outline = outline,
+                 names=varnames, plot = plot, border = border, col = col, ann = ann, horizontal = horizontal, add = add)
+  for(i in 1:dimn) {
+    boxCI(x, CI = CIs, component = i,dimn = dimn,  
+          quan.color = quan.color, horizontal = horizontal)
+  }
+}
+
+## used in Smcmc
+chain_stacker <- function(x) {
+  m <- length(x)
+  
+  if(class(x) != "list")
+    stop("must be list of chains")
+  
+  if(is.null(x))
+    stop("Chains are null")
+  
+  n <- as.integer(nrow(x[[1]]))
+  p <- as.integer(ncol(x[[1]]))
+  
+  b.final <- floor(mean(sapply(x, batchSize))) # mean batch size
+  
+  a <- floor(n/b.final)
+  ab <- a*b.final
+  trash <- n-ab
+  big.chain <- matrix(0,ncol = p, nrow = ab*m)
+  colnames(big.chain) <- colnames(x[[1]])
+  if(ab != n)
+  {
+    for (i in 1:m) {
+      big.chain[((i-1)*ab+1):(i*ab),] <- x[[i]][-(1:trash),]
+    }
+  }else{
+    big.chain <- Reduce("rbind", x)
+  }
+  return(list("b.size" = b.final, "stacked.data" = big.chain))
+}
+
+## Layout for all 3 plotting function
+setLayout_trace <- function(p, ask = FALSE)
+{
+  if(p <= 4)
+  {
+    mfrow <- c(p,1)
+  }
+  else if(p%%2 != 0)
+  {
+    mfrow = c((p+1)/2,2)
+  }
+  
+  else
+  {
+    mfrow = c(p/2,2)
+  }
+  par(ask = FALSE, mfrow = mfrow)
+  k = list(ask,mfrow)
+  names(k) = c("ask","mfrow")
+  return(k)
+}
+
+## Bandwidth
+bndw <- function(x) {
+  x <- x[!is.na(as.vector(x))]
+  return(1.06 * min(sd(x), IQR(x)/1.34) * length(x)^-0.2)
+}
+
+
+## plotting PMF in discrete chain case
+plot_pmf <- function(q, p, ylab, main= NA, which= NULL) {
+  
+  plot(q, p, type = "h", xlab = "x", ylab = ylab, ylim = c(-0.003,max(p)+0.01),main = if(is.null(which)){NA} else{main})
+  points(q, p, pch = 16, cex = 1.2, col = "palevioletred3")
+  abline(a = 0,b=0,h=T,lwd =0.1,lty=1)
+  mp = max(p)
+  mq = q[which(p == mp)]
+  mp = rep(mp,length(mq))
+  points(mq, mp, pch = 16, cex = 1.2, col = "steelblue")
+}
